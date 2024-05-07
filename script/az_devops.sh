bac1089bab1953b08c173c6f77f88875258d37df
An Tran <atra@gmail.com>
2024-05-07 10:53:13 +0200

    improve az_devops script

--- script/az_devops.sh
+++ script/az_devops.sh
@@ -4,11 +4,9 @@ cd "$(dirname "$0")"
 read -p "Service Principal ID: " sp_id
 read -p "Service Principal secret: " sp_key
 read -p "AZURE_DEVOPS_EXT_GITHUB_PAT: " AZURE_DEVOPS_EXT_GITHUB_PAT
+read -p "Az DevOps Org name: " org
 
-export AZURE_DEVOPS_EXT_GITHUB_PAT=$AZURE_DEVOPS_EXT_GITHUB_PAT
-
-
-org_url="https://dev.azure.com/myAKSLabOrg"
+org_url="https://dev.azure.com/$org"
 project=$(az devops project list --org $org_url --query "value[0].name" -o tsv)
 service_connection_az="az-svc-connection"
 service_connection_github="github-svc-connection"
@@ -21,29 +19,44 @@ rg_name=$(az group list --query "[].name" -o tsv)
 # Set service principal secret using env var
 export AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY=$sp_key
 
+# Set Default DevOps Organisation and Project:
+az devops configure --defaults organization=$org_url project=$project
+
 # Create service connection
-echo "Creating service connection to Azure: $service_connection"
-az devops service-endpoint azurerm create --azure-rm-service-principal-id $sp_id\
-    --azure-rm-subscription-id $subscription_id \
-    --azure-rm-subscription-name "$subscription_name" \
-    --azure-rm-tenant-id $tenantId \
-    --name $service_connection \
-    --org $org_url --project $project
+connection_exist=$(az devops service-endpoint list --query "[].name" -o tsv | grep $service_connection_az)
+if [ -z "$connection_exist" ]
+then
+    echo "Creating service connection to Azure: $service_connection_az"
+    az devops service-endpoint azurerm create --azure-rm-service-principal-id $sp_id \
+        --azure-rm-subscription-id $subscription_id \
+        --azure-rm-subscription-name "$subscription_name" \
+        --azure-rm-tenant-id $tenantId \
+        --name $service_connection_az
+else
+    echo "$service_connection_az already exist"
+fi
+
+
 
 echo "Creating service connection to GitHub: $service_connection_github"
-az devops service-endpoint github create --github-url $repo --name $service_connection_github \
-    --org $org_url --project $project
+export AZURE_DEVOPS_EXT_GITHUB_PAT=$AZURE_DEVOPS_EXT_GITHUB_PAT
+az devops service-endpoint github create --github-url $repo --name $service_connection_github
 
 # Create a pipeline
 PL_name="PL_Terraform_Create_AKS"
-backendAzureRmStorageAccountName=$(az storage account list -g $rg_name --query "[].name" -o tsv | grep "tfstate*")
-
-az pipelines create --org $org_url --project $project --name $PL_name \
-    --repository $repo \
-    --service-connection 
+pipeline_exist=$(az pipelines list --query "[].name" -o tsv | grep $PL_name)
+if [ -z $pipeline_exist]
+then
+    az pipelines create --org $org_url --project $project --name $PL_name \
+        --repository $repo --yml-path "pipelines/PL-Deploy-AKS.yaml" \
+        --service-connection $service_connection_az
+else
+    echo "$PL_name already exist"
+fi
 
 # Run the pipeline
-az pipelines run --org $org_url --project $project --open true --name $PL_name \
-    --variables "backendAzureRmResourceGroupName=$rg_name \
-    backendAzureRmStorageAccountName=$backendAzureRmStorageAccountName"
+# backendAzureRmStorageAccountName=$(az storage account list -g $rg_name --query "[].name" -o tsv | grep "tfstate*")
+# az pipelines run --org $org_url --project $project --open true --name $PL_name \
+#     --variables "backendAzureRmResourceGroupName=$rg_name \
+#     backendAzureRmStorageAccountName=$backendAzureRmStorageAccountName"
     
