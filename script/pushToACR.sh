80830eca6ca98de4b0daa96ddf71d2850fc30663
An Tran <atra@gmail.com>
2024-05-09 21:17:45 +0200

    fix script

--- script/pushToACR.sh
+++ script/pushToACR.sh
@@ -4,5 +4,9 @@ cd $(dirname "$0")
 acr=$(az acr list --query "[].loginServer" -o tsv)
 az acr login --name $acr
 
-docker build -t $acr/dotnet/sampleapp:latest .
+# Remove image if exist
+existing_image=$(docker images | grep sampleapp | awk '{print $1}')
+docker rmi $existing_image
+
+docker build -t $acr/dotnet/sampleapp:latest ../aspnet-core-dotnet-core/Dockerfile
 docker push $acr/dotnet/sampleapp:latest
