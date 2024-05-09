f380f32182e973d3c7a0d0007af157012dc6b838
An Tran <atra@gmail.com>
2024-05-09 20:28:05 +0200

    Edit k8s manifest

--- script/deploy_to_aks.sh
+++ script/deploy_to_aks.sh
@@ -4,5 +4,7 @@ set -x
 
 cd ./terraform
 # Create secret
-instrumentation_key=$(terraform output instrumentation_key)
+instrumentation_key=$(terraform output instrumentation_key | tr -d '"')
 kubectl -n default create secret generic aikey --from-literal=aisecret=$instrumentation_key
+
+kubectl apply -f ../k8s_manifest/aspnet.yaml 
